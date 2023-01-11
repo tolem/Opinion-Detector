e7d4f02bb95cc36492d88c4c868f94b358438f8c
Lami <olamide2olowoniyi@gmail.com>
2023-01-11 02:00:51 -0500

    added files

--- Opinion Detector/task/detector.py
+++ Opinion Detector/task/detector.py
@@ -5,8 +5,7 @@ file_name = input()
 line_number = 0
 with open('%s' % file_name, 'r', encoding="UTF-8") as file:
     while line_number < 150000:
-        line = file.readline()
-        line = line.strip()
+        line = file.readline().strip()
         line_number = line_number + 1
         if len(line) > 0:
             score = line[-2:] if line[-2].isdigit() else line[-1:]
