10bcbff04c24d808d757334262211a6c82897951
Lami <olamide2olowoniyi@gmail.com>
2023-01-15 03:00:13 -0500

    added rule bsed approach

--- Opinion Detector/task/detector.py
+++ Opinion Detector/task/detector.py
@@ -3,6 +3,8 @@ import pandas as pd
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+
 # nltk.download('stopwords')
 
 english_stopwords = set(stopwords.words('english'))
@@ -10,7 +12,7 @@ opinion = {"Review": [], "Score": []}
 file_name = input()
 line_number = 0
 lemmatizer = WordNetLemmatizer()
-df = pd.DataFrame(data=opinion, dtype=str)
+df = pd.DataFrame(data=opinion)
 # # df.info()
 
 with open('%s' % file_name, 'r', encoding="UTF-8") as file:
@@ -42,7 +44,7 @@ with open('%s' % file_name, 'r', encoding="UTF-8") as file:
                 yield i
 
         def add_one(sent):
-            with open('positive_words.txt', 'r') as positive:
+            with open('file/positive_words.txt', 'r') as positive:
                 pos_file = set({w.strip() for w in positive.readlines()})
                 # print(pd.Series(['sister goodwill']).isin(pos_file))
                 count = sum([1 for s in sent if s in pos_file])
@@ -50,20 +52,15 @@ with open('%s' % file_name, 'r', encoding="UTF-8") as file:
                 # exit(1)
                 return count
 
-
-
         def sub_one(sent):
-            with open('negative_words.txt', 'r') as negative:
+            with open('file/negative_words.txt', 'r') as negative:
                 neg_file = set({w.strip() for w in negative.readlines()})
                 count = sum([1 for s in sent if s in neg_file])
                 return count
 
-        # print(add_one().head())
-
-        df['Score'] = df['Score'] + (df['Lemmas'].apply(lambda x: add_one(x)))
+        df['Score'] = df['Lemmas'].apply(lambda x: add_one(x))
         df['Score'] = df['Score'] - (df['Lemmas'].apply(lambda x: sub_one(x)))
-        # print(df['Lemmas'].apply(lambda x: add_one(x)).head())
-        print(df['Score'].head())
+        # print(df['Score'].head())
 
         def sentiment_tag(mark):
             if -3 <= mark <= 3:
@@ -76,7 +73,21 @@ with open('%s' % file_name, 'r', encoding="UTF-8") as file:
                 return "Negative"
 
 
-        # df['Lexicon-based sentiment'] = df['Score'].apply(sentiment_tag)
+        df['Lexicon-based sentiment'] = df['Score'].apply(lambda x: sentiment_tag(x))
+        explode = [0.0, 0.08, 0.0]
+        colors = ['saddlebrown', 'wheat', 'crimson']
+        df.groupby('Lexicon-based sentiment').size().plot( kind='pie', autopct='%.1f')
+        plt.show()
 
+        neutral = df['Lexicon-based sentiment'][df['Lexicon-based sentiment'] == 'Neutral'].count() / df['Lexicon-based sentiment'].count()
+        positive = df['Lexicon-based sentiment'][df['Lexicon-based sentiment'] =='Positive'].count() / df['Lexicon-based sentiment'].count()
+        negative = df['Lexicon-based sentiment'][df['Lexicon-based sentiment'] == 'Negative'].count() / df['Lexicon-based sentiment'].count()
+        difference = (positive - negative) * 100
+        val = {'Neutral': neutral, 'Positive': positive, 'Negative': negative}
+        max_percent = max(val, key=val.get)
+        min_percent = min(val, key=val.get)
+        min_percent = val[min_percent] * 100
 
-
+        print(f"The answer to the 1st question: {max_percent}")
+        print(f"The answer to the 2nd question: {int(min_percent)}%")
+        print(f"The answer to the 3rd question: {int(difference)}%")
