818ff5e1cd2d1088bd7e325e0ce25812422134c6
Lami <olamide2olowoniyi@gmail.com>
2023-01-16 16:24:39 -0500

    added rule bsed approach

--- Opinion Detector/task/detector.py
+++ Opinion Detector/task/detector.py
@@ -1,9 +1,16 @@
 # write yor code here
 import pandas as pd
-import nltk
+from nltk import regexp_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.svm import SVC
 
 # nltk.download('stopwords')
 
@@ -28,7 +35,7 @@ with open('%s' % file_name, 'r', encoding="UTF-8") as file:
 
     else:
         def filter_word(text):
-            ftext = nltk.regexp_tokenize(text, r'[A-z]+')
+            ftext = regexp_tokenize(text, r'[A-z]+')
             ftext = " ".join(ftext).lower()
             ftext = lemmatizer.lemmatize(ftext, pos='n')
             filterd_review = [lemmatizer.lemmatize(word, pos='n') for word in ftext.split() if word not in english_stopwords]
@@ -39,55 +46,44 @@ with open('%s' % file_name, 'r', encoding="UTF-8") as file:
         # print(df['Lemmas'].head(), df['Lemmas'].tail(), sep='\n')
 
 
-        def gen(w):
-            for i in w:
-                yield i
-
-        def add_one(sent):
-            with open('file/positive_words.txt', 'r') as positive:
-                pos_file = set({w.strip() for w in positive.readlines()})
-                # print(pd.Series(['sister goodwill']).isin(pos_file))
-                count = sum([1 for s in sent if s in pos_file])
-                # print(count, pos_file)
-                # exit(1)
-                return count
-
-        def sub_one(sent):
-            with open('file/negative_words.txt', 'r') as negative:
-                neg_file = set({w.strip() for w in negative.readlines()})
-                count = sum([1 for s in sent if s in neg_file])
-                return count
-
-        df['Score'] = df['Lemmas'].apply(lambda x: add_one(x))
-        df['Score'] = df['Score'] - (df['Lemmas'].apply(lambda x: sub_one(x)))
-        # print(df['Score'].head())
-
         def sentiment_tag(mark):
-            if -3 <= mark <= 3:
-                return "Neutral"
-
-            if mark > 3:
+            if 7 <= mark <= 10:
                 return "Positive"
 
-            if mark < -3:
+            if 1 <= mark <= 4:
                 return "Negative"
 
 
+        vectorizer = TfidfVectorizer()
         df['Lexicon-based sentiment'] = df['Score'].apply(lambda x: sentiment_tag(x))
-        explode = [0.0, 0.08, 0.0]
-        colors = ['saddlebrown', 'wheat', 'crimson']
-        df.groupby('Lexicon-based sentiment').size().plot( kind='pie', autopct='%.1f')
-        plt.show()
+        dataset = df['Lemmas'].tolist()
+        dataset = [" ".join(li) for li in dataset]
+        tfidf_matrix = vectorizer.fit_transform(dataset)
+        X, y = tfidf_matrix, df['Lexicon-based sentiment']
+        rS = RandomOverSampler()
+        X, y = rS.fit_resample(X, y)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, random_state=42)
 
-        neutral = df['Lexicon-based sentiment'][df['Lexicon-based sentiment'] == 'Neutral'].count() / df['Lexicon-based sentiment'].count()
-        positive = df['Lexicon-based sentiment'][df['Lexicon-based sentiment'] =='Positive'].count() / df['Lexicon-based sentiment'].count()
-        negative = df['Lexicon-based sentiment'][df['Lexicon-based sentiment'] == 'Negative'].count() / df['Lexicon-based sentiment'].count()
-        difference = (positive - negative) * 100
-        val = {'Neutral': neutral, 'Positive': positive, 'Negative': negative}
-        max_percent = max(val, key=val.get)
-        min_percent = min(val, key=val.get)
-        min_percent = val[min_percent] * 100
+        model = SGDClassifier(alpha=5e-6, fit_intercept=True, learning_rate='optimal')
+        model.fit(X_train, y_train)
+        y_predicts = model.predict(X_test)
+        print(classification_report(y_test, y_predicts))
+# import pandas as pd
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.metrics import classification_report
+#
+#
+# def sentiment_process(sc):
+#     if sc > 5:
+#         return 1
+#     return 0
+#
+#
+# input_ = input()
+# # path = "data.pkl"
+# # data = pd.read_pickle(input_)
+# data = pd.read_csv(input_, nrows=150000)
 
-        print(f"The answer to the 1st question: {max_percent}")
-        print(f"The answer to the 2nd question: {int(min_percent)}%")
-        print(f"The answer to the 3rd question: {int(difference)}%")
+
